ed1e43301dce647c5c7f810d46cea9f2fec420d9
David G Chaves <davidgchaves@gmail.com>
2015-08-17 12:39:29 +0200

    Add webpack-dev-server to webpack config
      - Uses webpack-dev-server to enable:
        * Hot Module Replacement (HMR).
        * HTML5 History API fallback.
      - Config npm to start the whole process with just:
        ✔ npm start
      - Point your browser to localhost:4000 to play with the App.

--- app/h1-component.js
+++ app/h1-component.js
@@ -1,5 +1,5 @@
 module.exports = function () {
   var h1 = document.createElement('h1');
-  h1.innerHTML = 'Hello World';
+  h1.innerHTML = 'Hello World with HMR';
   return h1;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
-var path = require('path');
+var path              = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var webpack           = require('webpack');
 
 var ROOT_PATH = path.resolve(__dirname);
 
@@ -9,7 +10,16 @@ module.exports = {
     path: path.resolve(ROOT_PATH, 'build'),
     filename: 'bundle.js'
   },
+  devServer: {
+    port: 4000,
+    colors: true,
+    historyApiFallback: true,
+    hot: true,
+    inline: true,
+    progress: true
+  },
   plugins: [
+    new webpack.HotModuleReplacementPlugin(),
     new HtmlWebpackPlugin({ title: 'Kanban Board' })
   ]
 }
