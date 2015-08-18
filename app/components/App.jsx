f96a3817821085a59e88f82d0a77a9ff9d3c89b0
David G Chaves <davidgchaves@gmail.com>
2015-08-18 13:03:23 +0200

    Refactor: use constructor to mantain App own state
      - State in React components:
        * can accept props from parent
        * can have state of their own, wired up in constructor

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -3,21 +3,28 @@ import React from 'react';
 import Notes from './Notes.jsx';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      notes: [
+        {
+          id: uuid.v4(),
+          task: 'Go pick amazon package'
+        },
+        {
+          id: uuid.v4(),
+          task: 'Find a nice place to eat'
+        },
+        {
+          id: uuid.v4(),
+          task: 'Try the new Memopulse MVP'
+        }
+      ]
+    };
+  }
   render() {
-    const notes = [
-      {
-        id: uuid.v4(),
-        task: 'Go pick amazon package'
-      },
-      {
-        id: uuid.v4(),
-        task: 'Find a nice place to eat'
-      },
-      {
-        id: uuid.v4(),
-        task: 'Try the new Memopulse MVP'
-      }
-    ];
+    const notes = this.state.notes;
     return (
       <div>
         <Notes items={notes} />
