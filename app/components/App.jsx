2652ce1a34c748aeb8023c8928f9fdc119a34547
David G Chaves <davidgchaves@gmail.com>
2015-08-18 17:18:25 +0200

    Simplify App component using Alt (Flux Architecture)

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,24 +1,28 @@
-import 'array.prototype.findIndex';
-import uuid from 'node-uuid';
 import React from 'react';
 import Notes from './Notes.jsx';
+import NoteActions from '../actions/NoteActions';
+import NoteStore from '../stores/NoteStore';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      notes: [
-        { id: uuid.v4(), task: 'Go pick amazon package' },
-        { id: uuid.v4(), task: 'Find a nice place to eat' },
-        { id: uuid.v4(), task: 'Try the new Memopulse MVP' }
-      ]
-    };
-
-    this.addNote    = this.addNote.bind(this);
-    this.editNote   = this.editNote.bind(this);
-    this.deleteNote = this.deleteNote.bind(this);
+    this.storeChanged = this.storeChanged.bind(this);
+    this.state = NoteStore.getState();
   }
+
+  componentDidMount() {
+    NoteStore.listen(this.storeChanged);
+  }
+
+  componentWillUnmount() {
+    NoteStore.unlisten(this.storeChanged);
+  }
+
+  storeChanged(state) {
+    this.setState(state);
+  }
+
   render() {
     const notes = this.state.notes;
 
@@ -32,40 +36,16 @@ export default class App extends React.Component {
       </div>
     );
   }
+
   addNote() {
-    this.setState({
-      notes: this.state.notes.concat([{
-        id: uuid.v4(),
-        task: 'New task'
-      }])
-    });
+    NoteActions.create({task: 'New task'});
   }
+
   editNote(id, task) {
-    let notes = this.state.notes;
-    const noteIndex = this.findNote(id);
-
-    if (noteIndex < 0) { return; }
-
-    notes[noteIndex].task = task;
-
-    this.setState({notes});
+    NoteActions.update({id, task});
   }
+
   deleteNote(id) {
-    const notes = this.state.notes;
-    const noteIndex = this.findNote(id);
-
-    if (noteIndex < 0) { return; }
-
-    this.setState({
-      notes: notes.slice(0, noteIndex).concat(notes.slice(noteIndex + 1))
-    });
-  }
-  findNote(id) {
-    const notes = this.state.notes;
-    const noteIndex = notes.findIndex((note) => note.id === id);
-
-    if (noteIndex < 0) { console.warn('Failed to find note', notes, id); }
-
-    return noteIndex;
+    NoteActions.delete(id);
   }
 }
