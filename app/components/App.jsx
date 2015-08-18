70a65f5e4917ef1d879aac546c8d34b0cdf28f7f
David G Chaves <davidgchaves@gmail.com>
2015-08-18 12:21:02 +0200

    Add basic Parent (App) / Child (Note) relationship
      - Use node-uuid module (from Node)
        ✔ npm install node-uuid --save
      - Use {} to mix JavaScript syntax within JSX
        <ul>{notes.map(this.renderNote)}</ul>
      - Use the `key` prop to tell React about rendering order
        https://facebook.github.io/react/docs/multiple-components.html
      - Parent ---> Child communication through React `props`
        (from parent) <Note task={note.task} />
        (from child)  <div>{this.props.task}</div>

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,8 +1,35 @@
+import uuid from 'node-uuid';
 import React from 'react';
 import Note from './Note.jsx';
 
 export default class App extends React.Component {
   render() {
-    return <Note />;
+    const notes = [
+      {
+        id: uuid.v4(),
+        task: 'Go pick amazon package'
+      },
+      {
+        id: uuid.v4(),
+        task: 'Find a nice place to eat'
+      },
+      {
+        id: uuid.v4(),
+        task: 'Try the new Memopulse MVP'
+      }
+    ];
+    return (
+      <div>
+        <ul>{notes.map(this.renderNote)}</ul>
+      </div>
+    );
+  }
+
+  renderNote(note) {
+    return (
+      <li key={`note${note.id}`}>
+        <Note task={note.task} />
+      </li>
+    );
   }
 }
